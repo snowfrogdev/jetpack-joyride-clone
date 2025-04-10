70c4045a66e95d5bc86ab2aae12a95045b4dcbda
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-04-10 16:37:59 -0400

    Enhance player mechanics with hurtbox detection and update SunCoreRelays dimensions

--- player.gd
+++ player.gd
@@ -6,6 +6,11 @@ extends CharacterBody2D
 @export var max_fall_speed: float = 600.0
 @export var damping: float = 0.3
 
+@onready var hurtbox: Area2D = $Hurtbox
+
+func _ready() -> void:
+  hurtbox.area_entered.connect(_on_hurtbox_area_entered)
+
 func _physics_process(delta: float) -> void:
   if not is_on_floor():
     velocity.y += gravity * delta
@@ -22,3 +27,7 @@ func _physics_process(delta: float) -> void:
   velocity.y = min(velocity.y, max_fall_speed)
 
   move_and_slide()
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+  if area.is_in_group("hazard"):
+    print("Player hit by hazard!")
