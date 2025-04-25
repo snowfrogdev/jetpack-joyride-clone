8c4ecb593e884bbaa36ab4234796c866f19e7c34
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-04-24 22:35:36 -0400

    Add player state management for flying and running
    - Implemented the flying state in `flying.gd`, including an enter function that plays the "REST" animation.
    - Created the running state in `running.gd`, with an enter function that plays the "RUN" animation at the specified speed.
    - Established a base class `PlayerState` in `player_state.gd` to manage common player state functionality.
    - Added necessary UID files for the new state scripts.

---
+++ player/states/player_state.gd
@@ -0,0 +1,15 @@
+class_name PlayerState extends State
+
+const RUNNING = "Running"
+const FLYING = "Dying"
+
+var player: Player
+var game: Game
+
+func _ready() -> void:
+  player = owner as Player
+  await owner.ready
+  assert(player != null, "PlayerState must be attached to the Player node.")
+  
+  game = %Game as Game
+  assert(game != null, "Game node is not loaded.")
