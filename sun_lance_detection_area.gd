79e477ed6b1af759a46c4b3bef0f765f5c9030bf
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-05-09 20:16:20 -0400

    Refactor Sun Lance detection area: move attack spawning logic to a separate function for improved readability and maintainability

--- sun_lance_detection_area.gd
+++ sun_lance_detection_area.gd
@@ -8,10 +8,13 @@ func _ready() -> void:
 func _on_area_entered(area: Area2D) -> void:
     if area is SunLanceTrigger:
         var detection_x = _get_detection_line_x()
-        var attack = sun_lance_attack_scene.instantiate()
-        attack.position = global_position
-        attack.init(area, detection_x)
-        get_parent().add_child(attack)
+        call_deferred("_spawn_attack", area, detection_x)
+
+func _spawn_attack(area: Area2D, detection_x: float) -> void:
+    var attack = sun_lance_attack_scene.instantiate()
+    attack.position = global_position
+    attack.init(area, detection_x)
+    get_parent().add_child(attack)
 
 func _get_detection_line_x() -> float:
     var shape_node := get_node("CollisionShape2D") as CollisionShape2D
