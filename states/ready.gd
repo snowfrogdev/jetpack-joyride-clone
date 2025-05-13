27dfd6569d4533791669438b20490518bacafd68
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-05-13 15:05:46 -0400

    Add intro music and boost prompt label to game states with audio transition handling

--- states/ready.gd
+++ states/ready.gd
@@ -1,15 +1,28 @@
 extends GameState
 
+@onready var intro_music = get_node("/root/Main/IntroMusic")
+@onready var game_music = get_node("/root/Main/Music")
+
 func enter(_prev: String, _data: Dictionary = {}) -> void:
   if OS.has_feature("debug"):
     print("Entering Ready state")
   
   game.load_best_distance()
   get_tree().paused = true
+  
+  # Show the boost prompt label
+  get_node("/root/Main/UI/BoostPromptLabel").visible = true
+  
+  # Stop game music if it's playing and play intro music
+  game_music.stop()
+  intro_music.play()
 
 func update(_delta) -> void:
   if Input.is_action_just_pressed("Boost"):
     if OS.has_feature("debug"):
       print("Exiting Ready state")
-
+    
+    # Hide the boost prompt label
+    get_node("/root/Main/UI/BoostPromptLabel").visible = false
+    
     emit_signal("finished", "Running")
--- states/running.gd
+++ states/running.gd
@@ -1,15 +1,52 @@
 extends GameState
 
+@onready var intro_music = get_node("/root/Main/IntroMusic")
+@onready var game_music = get_node("/root/Main/Music")
+@export var crossfade_duration: float = 1.0
+
+var transition_timer: float = 0.0
+var transitioning: bool = false
+var initial_intro_volume: float
+var initial_game_volume: float
+
 func enter(_prev: String, _data: Dictionary = {}) -> void:
   if OS.has_feature("debug"):
     print("Entering Running state")
   get_tree().paused = false
   game.dying.connect(_on_player_dying)
+  
+  # Start music transition
+  initial_intro_volume = intro_music.volume_db
+  initial_game_volume = game_music.volume_db
+  
+  # Start with game music at very low volume
+  game_music.volume_db = -80.0
+  game_music.play()
+  
+  transitioning = true
+  transition_timer = 0.0
 
 func exit() -> void:
   if OS.has_feature("debug"):
     print("Exiting Running state")
   game.dying.disconnect(_on_player_dying)
+
+func update(delta: float) -> void:
+  if transitioning:
+    transition_timer += delta
+    var t = min(transition_timer / crossfade_duration, 1.0)
+    
+    # Exponential fade for smoother transition
+    var fade_out_value = lerp(initial_intro_volume, -80.0, t * t)
+    var fade_in_value = lerp(-80.0, initial_game_volume, t * t)
+    
+    intro_music.volume_db = fade_out_value
+    game_music.volume_db = fade_in_value
+    
+    if t >= 1.0:
+      transitioning = false
+      intro_music.stop()
+      intro_music.volume_db = initial_intro_volume
 
 func _on_player_dying() -> void:
   finished.emit(DYING)
