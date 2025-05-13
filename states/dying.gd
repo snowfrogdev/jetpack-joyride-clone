4cc35294572b8ee9e698dddc7abfd067c8fd5b0a
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-05-13 15:51:58 -0400

    Add game over audio jingle and restart prompt handling in game over state

--- states/dying.gd
+++ states/dying.gd
@@ -1,10 +1,20 @@
 extends GameState
 
+@onready var game_music = get_node("/root/Main/Music")
+@onready var game_over_jingle = get_node("/root/Main/GameOverJingle")
+
 func enter(_prev: String, _data: Dictionary = {}) -> void:
   if OS.has_feature("debug"):
     print("Entering Dying state")
   game.died.connect(_on_player_dead)
   game.set_speed(0)
+  
+  # Stop the game music before playing the game over jingle
+  game_music.stop()
+  
+  # Play the game over jingle
+  if game_over_jingle:
+    game_over_jingle.play()
 
 func exit() -> void:
   if OS.has_feature("debug"):
--- states/game_over.gd
+++ states/game_over.gd
@@ -1,14 +1,27 @@
 extends GameState
 
+@onready var restart_label = get_node("/root/Main/UI/RestartPromptLabel")
+
 func enter(_prev: String, _data: Dictionary = {}) -> void:
   if OS.has_feature("debug"):
     print("Entering GameOver state")
   
-  get_tree().paused = true
+  # Don't pause the entire tree, instead just pause gameplay elements
+  # This allows audio to continue and input events to be processed
+  game.set_speed(0) # Stop the game's scrolling
+  
+  # Show the restart label
+  restart_label.visible = true
+  
+  # Save best distance
   game.save_best_distance()
 
+func exit() -> void:
+  # Hide the restart label when exiting the state
+  restart_label.visible = false
+
 func handle_input(event: InputEvent) -> void:
-  if event.is_action_pressed("ui_accept"):
+  if event.is_action_pressed("ui_accept") or (event is InputEventMouseButton and event.pressed and event.button_index == 1):
     if OS.has_feature("debug"):
       print("Exiting GameOver state")
     get_tree().reload_current_scene()
